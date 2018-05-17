142c091c3578abfa44e377c0ca2a9a6d926b7698
zenthangplus <zenthangplus@gmail.com>
2018-05-17 13:58:42 +0700

    Fix reset badge

--- webview.js
+++ webview.js
@@ -4,18 +4,19 @@ module.exports = (Franz) => {
         if ($imgCounts.length > 0) {
             const $img = $imgCounts[0];
             const src = $img.getAttribute('src');
+            let count = 0;
             if (src) {
                 const regex = /message_notification_(.+).png$/g;
                 const execResult = regex.exec(src);
                 if (execResult.length > 0) {
-                    let count = execResult[1];
+                    count = execResult[1];
                     if (count === 'more') {
                         count = 6;
                     }
-                    // set Franz badge
-                    Franz.setBadge(parseInt(count));
                 }
             }
+            // set Franz badge
+            Franz.setBadge(parseInt(count));
         }
     };
 
